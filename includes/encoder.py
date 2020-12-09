f0fd52261b22c0367a9ab4ad0fb0c428992046af
Tanja84dk <tanja84dk@gmail.com>
2020-12-09 02:07:58 +0100

    Cleaning

--- includes/encoder.py
+++ includes/encoder.py
@@ -6,6 +6,6 @@ def encode_base64(x):
     with open(x, 'r') as base64_decoded:
         message = base64_decoded.read()
         message_bytes = message.encode('ascii')
-        base64_bytes = base64.b64encode(message_bytes, altchars=None)
+        base64_bytes = base64.b64encode(message_bytes)
         base64_message = base64_bytes.decode('ascii')
     print(base64_message)
