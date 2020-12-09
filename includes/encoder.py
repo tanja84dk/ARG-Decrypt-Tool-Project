7c1a3b0c13024b21e4f32728ecf1897217bcd614
Tanja84dk <tanja84dk@gmail.com>
2020-12-09 10:52:46 +0100

    Allow to encode from txt file

--- includes/encoder.py
+++ includes/encoder.py
@@ -11,6 +11,17 @@ def encode_base64(x):
         base64_message = base64_bytes.decode('ascii')
     print(base64_message)
 
+def encode_file_base64(x, y):
+    now = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+    with open(x, 'r') as pre_encode:
+        pre_encode_message = pre_encode.read()
+        pre_encode_bytes = pre_encode_message.encode('ascii')
+        message_bytes = base64.b64encode(pre_encode_bytes)
+        message = message_bytes.decode('ascii')
+        with open(now + '-' + y, 'w') as base64_encoded:
+            base64_encoded.write(message)
+        print(message)
+
 def encode_input_base64(x, y):
     now = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
     message = x
