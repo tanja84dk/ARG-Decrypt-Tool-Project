c32362f8e9125308f47527d07e182fc2c25e1f95
Tanja84dk <tanja84dk@gmail.com>
2020-12-09 03:58:33 +0100

    added sub menu

--- includes/decoder.py
+++ includes/decoder.py
@@ -9,3 +9,18 @@ def decoder_base64(x):
         message_bytes = base64.b64decode(base64_bytes)
         message = message_bytes.decode('ascii')
         print(message)
+
+def decode_file_base64(x):
+    with open(x, 'r') as base64_encoded:
+        base64_message = base64_encoded.readline(1)
+        base64_bytes = base64_message.encode('ascii')
+        message_bytes = base64.b64decode(base64_bytes)
+        message = message_bytes.decode('ascii')
+        print(message)
+
+def decode_input_base64():
+    base64_message = input("Enter String: ")
+    base64_bytes = base64_message.encode('ascii')
+    message_bytes = base64.b64decode(base64_bytes)
+    message = message_bytes.decode('ascii')
+    print(message)
--- main.py
+++ main.py
@@ -17,7 +17,24 @@ while option != 0:
         encoder.encode_base64("decoded.txt")
     elif option == 2:
         print("Decoding")
-        decoder.decoder_base64("encoded.txt")
+        # decoder.decoder_base64("encoded.txt")
+        
+        print(" [1] From input: ")
+        print(" [2] From File: ")
+        
+        option_decode = int(input("Choose Decoding: "))
+        while option_decode != 0:
+            if option_decode == 1:
+                print("Encoded String: ")
+                decoder.decode_input_base64()
+                option_decode = 0
+            elif option_decode == 2:
+                print("2. From File")
+                decoder.decode_file_base64("encoded.txt")
+                option_decode = 0
+            else:
+                print("Issue")
+
     elif option == 3:
         print("Number 3")
     elif option == 4:
