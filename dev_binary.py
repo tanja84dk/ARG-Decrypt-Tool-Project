aa0f29647bdd033027ab81bb6ce7bb0736021ad1
Tanja84dk <tanja84dk@gmail.com>
2020-12-11 23:18:24 +0100

    Cracked the code for multi line decoding

--- dev_binary.py
+++ dev_binary.py
@@ -25,15 +25,10 @@ with open("binaryin.plain", 'r') as file:
 #        print(line + "\n")
         line_new = line.replace(" ", "\n")
 #        print(line_new)
-        print(type(line_new))
+#        print(type(line_new))
         line_new = list(line_new.split("\n"))
-        print(type(line_new))
+#        print(type(line_new))
         test = bits2string(line_new)
         print(test)
-#    print(new)
-#    count = 0
-#    for line in Lines:
-#        print(line.strip())
-#        print(list(line.strip()))
-#    for line in Lines:
-#        print("Line{}: {}".format(count, line.strip()))
+        with open(f"output/{ now }-binarydecoded.txt", 'a+') as writefile:
+            writefile.write(test + "\n\n")
