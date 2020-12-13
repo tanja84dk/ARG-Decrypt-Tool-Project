cf93082d1929054784d47e16837773102f82b749
Tanja84dk <tanja84dk@gmail.com>
2020-12-13 12:01:51 +0100

    Fixed Debug func to add date and time to filename

--- debugfiles/debug.py
+++ debugfiles/debug.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 
-def debugging(x, y):
-    message = y
-    with open("debug-" + x, 'w') as debug_log:
+import datetime
+
+def debugging(filename, debugmessage):
+    "Writing Output to a log file to the folder for debugging. extension .log is added automaticly. filename is added to filename to easier locating. Output file is prepended with date and time added to avoid overwrite"
+    now = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+    message = str(debugmessage)
+    with open("debuglogs/" + now + "-debug-" + filename + ".log", 'w+') as debug_log:
             debug_log.write(message)
