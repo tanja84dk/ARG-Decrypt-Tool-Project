06fca75bb14fbd8d6680d9487c4a1f413b0397c9
Tanja84dk <tanja84dk@gmail.com>
2020-12-13 01:32:38 +0100

    Fixed Main Comments And A Few Variables

--- includes/ArgTool_base64.py
+++ includes/ArgTool_base64.py
@@ -3,19 +3,20 @@
 import base64
 import datetime
 
-def encode_file_base64(x, y):
+def encode_file_base64(input_file, output_file):
+    "Plain Text is the indput. And output_file is output filename. Output file is prepended with date and time added to avoid overwrite"
     now = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-    with open(x, 'r') as pre_encode:
+    with open(input_file, 'r') as pre_encode:
         pre_encode_message = pre_encode.read()
         pre_encode_bytes = pre_encode_message.encode('ascii')
         message_bytes = base64.b64encode(pre_encode_bytes)
         message = message_bytes.decode('ascii')
-        with open(now + '-' + y, 'w') as base64_encoded:
+        with open(now + '-' + output_file, 'w') as base64_encoded:
             base64_encoded.write(message)
         print(message)
 
 def encode_input_base64(plain_text, output_file):
-    "Plain Text is the indput. And output_file is output filename with date and time added to avoid overwrites"
+    "Plain Text is the indput. Output file is prepended with date and time added to avoid overwrite"
     now = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
     pre_encode_message = plain_text
     message_bytes = pre_encode_message.encode('ascii')
@@ -25,23 +26,25 @@ def encode_input_base64(plain_text, output_file):
         base64_input_encode.write(base64_message)
     print(base64_message)
 
-def decode_file_base64(x, y):
+def decode_file_base64(input_file, output_file):
+    "Base64 encoded text file is input. Output file is prepended with date and time added to avoid overwrite"
     now = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-    with open(x, 'r') as base64_encoded:
+    with open(input_file, 'r') as base64_encoded:
         base64_message = base64_encoded.readline().rstrip()
         base64_bytes = base64_message.encode('ascii')
         message_bytes = base64.b64decode(base64_bytes)
         message = message_bytes.decode('ascii')
-        with open(now + '-' + y, 'w') as base64_decoded:
+        with open(now + '-' + output_file, 'w') as base64_decoded:
             base64_decoded.write(message)
         print(message)
 
-def decode_input_base64(x, y):
+def decode_input_base64(string_input, output_file):
+    "Base64 encoded text is the input from string. Output file is prepended with date and time added to avoid overwrite"
     now = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-    base64_message = x
+    base64_message = string_input
     base64_bytes = base64_message.encode('ascii')
     message_bytes = base64.b64decode(base64_bytes)
     message = message_bytes.decode('ascii')
-    with open(now + '-' + y, 'w') as base64_input_decoded:
+    with open(now + '-' + output_file, 'w') as base64_input_decoded:
             base64_input_decoded.write(message)
     print(message)
